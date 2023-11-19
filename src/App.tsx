f676a3e163d482f2557cc203a78121956e54d30c
Félix Vicente <felixvicent1306@gmail.com>
2023-11-19 16:18:24 -0300

    feat: add felps

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello Felps</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((c) => c + 1)}>
           count is {count}
